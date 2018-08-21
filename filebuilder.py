f75a169ce35aca0a5347a87c6fa2632d3285484d
William Shaffer <wshaffer@waysysweb.com>
2018-08-20 20:57:41 -0400

    enhanced filebuilder

--- filebuilder.py
+++ filebuilder.py
@@ -23,6 +23,9 @@ This module holds the FileBuilder class.
 """
 
 from suitedreamsexception import SuiteDreamsException
+from testcase import TestCase
+from productspec import ProductSpec
+from pathlib import Path
 
 # -------------------------------------------------------------------------------
 #  SuiteBuilder class
@@ -41,11 +44,32 @@ class FileBuilder:
         Arguments:
             test_suite_library - the directory where the test suite will be placed
         """
-        assert producer_spec is not None, "FileBuilder:"
+        assert producer_spec is not None, "FileBuilder: Producer spec must not be None"
         assert test_suite_library is not None, "FileBuilder: test suite library must not be null"
         assert len(test_suite_library) > 0, "FileBuilder: test suit library must not be empty"
         self._producer_spec = producer_spec
         self._test_suite_library = test_suite_library
+
+    # ---------------------------------------------------------------------------
+    #  Properties
+    # ---------------------------------------------------------------------------
+
+    @property
+    def test_suite_library(self):
+        """Return the name of the directory that will hold the test suite directory"""
+        return self._test_suite_library
+
+    @property
+    def suite_name(self):
+        """Return the name for the test suite"""
+        name = self._producer_spec.suite_name
+        return name
+
+    @property
+    def test_suite_dir(self):
+        """Return the directory path where the test case files will reside"""
+        dir = self.test_suite_library + "/" + self.suite_name
+        return dir
 
     # ---------------------------------------------------------------------------
     #  Operations
@@ -55,4 +79,23 @@ class FileBuilder:
         """
         Generate a test case file in the test suite directory.
         """
+        self.validate_inputs()
+
+
         return
+
+    def validate_inputs(self):
+        """
+        Validate the data:
+            -- verify that the test suite library directory exists
+            -- create the test suite directory
+        """
+        path = Path(self.test_suite_library)
+        if not path.is_dir():
+            raise SuiteDreamsException("Test suite library is not a directory - " + self.test_suite_library)
+        path = Path(self.test_suite_dir)
+        if path.exists():
+            raise SuiteDreamsException("Test suite directory already exists - " + self.test_suite_dir)
+        path.mkdir(mode=0o777, parents=False, exist_ok=False)
+        return
+
